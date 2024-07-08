a0a4dc6b0348afbd034a596cedef7ea0626783c0
vknanavati <vimin.nanavati@gmail.com>
2024-07-08 17:07:28 -0400

    created state variable taskList.

--- src/App.js
+++ src/App.js
@@ -1,29 +1,34 @@
 import { useState } from 'react';
 function App() {
 
-  const [task, setTask] = useState("")
+  const [task, setTask] = useState("");
+  const [taskList, setTaskList] = useState("");
 
   const handleChange = e => {
     setTask(e.target.value)
   }
 
-  const handleClick = (e) => {
-
-
+  const handleClick = e => {
+    e.preventDefault();
+    setTaskList(task)
   }
   return (
     <>
       <h2>To Do List</h2>
-      <input
-        type="text"
-        value={task}
-        onChange={e => handleChange(e)}
-      />
-      <button
-        onClick={e => handleClick(e)}
-      >Add Task</button>
+      <form>
+        <input
+          type="text"
+          value={task}
+          onChange={e => handleChange(e)}
+        />
+        <button
+          onClick={e => handleClick(e)}
+        >Add Task</button>
+      </form>
+      <ul>
+        <li>{taskList}</li>
+      </ul>
 
-      <p>{task}</p>
     </>
   );
 }
