dc961ad7336d009710e9d60648a684a5110391a7
vknanavati <vimin.nanavati@gmail.com>
2024-07-10 21:44:29 -0400

    created function to delete task

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@
 import { useState } from 'react';
 function App() {
 
-  /*state variable that stores the task
-  entered in the input box*/
+  /* task is a state variable that stores the task
+  entered into the input box*/
   const [task, setTask] = useState("");
-  //array that stores the tasks
+  /*list is an array that stores the taskObject*/
   const [list, setList] = useState([]);
 
+  /*event handler for Add Task button */
   const handleClick = e => {
     e.preventDefault();
 
@@ -17,13 +18,24 @@ function App() {
       id: Math.floor(Math.random() * 1000),
       value: task
     };
-    //adds task to the list of tasks
+    /*adds new task added to the taskObject
+    to the list array*/
     setList([...list, taskObject]);
 
     console.log("useState variable 'list': ", list)
     console.log("taskObject:", taskObject)
 
+    /*sets useState variable task 
+    back to an empty string so user sees
+    empty input box after pressing the Add button*/
     setTask("");
+  }
+  /*event handler for the - button */
+  const removeTask = (e, id) => {
+    e.preventDefault();
+    const updatedList = list.filter((taskObject) => taskObject.id !== id);
+
+    setList(updatedList)
 
   }
   return (
@@ -42,12 +54,19 @@ function App() {
       <ul>
         {list.map(taskObject => {
           return (
-            <li
-              key={taskObject.id}
-            >
-              {taskObject.value}</li>
+            <>
+              <li
+                key={taskObject.id}
+              >
+                {taskObject.value}</li>
+              <button
+                onClick={(e) => removeTask(e, taskObject.id)}
+              >-</button>
+            </>
           )
-        })}
+        }
+        )
+        }
       </ul>
 
     </>
