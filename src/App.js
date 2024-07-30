c3d37498e4ae50e6a217a7f71b4684cd19eba254
vknanavati <vimin.nanavati@gmail.com>
2024-07-30 15:11:21 -0400

    disabled add task button based on user input.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import { useState } from 'react';
-import {Container, Typography, Grid, Button, TextField} from '@mui/material'
+import {Container, Typography, Grid, Button, TextField, Box} from '@mui/material'
 
 function App() {
   /* task is a state variable that stores the task
@@ -8,12 +8,14 @@ function App() {
   const [task, setTask] = useState("");
   /*list is an array that stores the taskObject*/
   const [list, setList] = useState([]);
+  const [errorMessage, setErrorMessage] = useState(true);
 
   /*event handler for Add Task button */
   const handleClick = e => {
     e.preventDefault();
 
     console.log(`Task variable: ${task}`)
+    console.log(`Task length: ${task.length}`)
 
     const taskObject = {
       id: Math.floor(Math.random() * 1000),
@@ -30,7 +32,23 @@ function App() {
     back to an empty string so user sees
     empty input box after pressing the Add button*/
     setTask("");
+    setErrorMessage(true)
   }
+
+  const handleChange = e => {
+    console.log("input text:>>> ", e)
+    if (e.length < 1) {
+      console.log('Input is less than 1')
+      setErrorMessage(true)
+    } else {
+      console.log('Input is more than 1')
+      setErrorMessage(false)
+    }
+
+    setTask(e)
+  }
+
+
   /*event handler for the - button */
   const removeTask = (e, id) => {
     e.preventDefault();
@@ -47,7 +65,7 @@ function App() {
       >
         <Typography
         variant="h5"
-        sx={{marginTop: 5, marginBottom: 2}}
+        sx={{marginTop: 5, marginBottom: 4}}
         >
           To Do List
         </Typography>
@@ -63,16 +81,31 @@ function App() {
                     width: 200,
                     "& .MuiInputBase-root":{
                       height: 45
-                    }
+                    },
+                    fieldset: {borderColor: "black"}
                   }}
-                type="text"
+                type={"text"}
                 value={task}
-                onChange={e => setTask(e.target.value)}
-              />
+                onChange={e => handleChange(e.target.value)}
+                />
             </Grid>
             <Grid item>
               <Button
-              variant="outlined"
+                sx={{
+                  border: "2px #4FBF8D solid",
+                  color: "#4FBF8D",
+                  "&:hover": {
+                    border: "2px #4FBF8D solid",
+                  },
+                  fontWeight: 700
+                }}
+                //
+                disabled={errorMessage}
+                helperText={errorMessage}
+                error={errorMessage}
+
+                //
+                variant="outlined"
                 onClick={e => handleClick(e)}
               >
                 Add Task
@@ -81,27 +114,40 @@ function App() {
           </Grid>
         </form>
         <ul>
-          <Grid
-            container
-            alignItems={"center"}
-
-          >
-          {list.map(taskObject => {
+          {list.map((taskObject, i) => {
             return (
               <>
-                <li
-                  key={taskObject.id}
-                  >
-                  {taskObject.value}</li>
-                <Button
-                  onClick={(e) => removeTask(e, taskObject.id)}
-                  variant="outlined"
+              <Container>
+                <Box
+                display={"flex"}
+                gap={2}
+                my={2}
+                alignItems={"center"}
                 >
-                  -
-                </Button>
+                  <li
+                    key={taskObject.id}
+                    >
+                    {taskObject.value}
+                  </li>
+                  <Button
+                    sx={{
+                      height:30,
+                      minWidth: 0,
+                      color: "#ff7474",
+                      border: "3px #ff7474 solid",
+                      "&:hover": {
+                        border: "3px #ff7474 solid",
+                      },
+                    }}
+                    onClick={(e) => removeTask(e, taskObject.id)}
+                    variant="outlined"
+                    >
+                    -
+                  </Button>
+                </Box>
+              </Container>
               </>
             )})}
-          </Grid>
         </ul>
       </Grid>
     </Container>
